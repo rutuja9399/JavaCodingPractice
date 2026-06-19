49dd767e9d80f59e584a164cb7528562a3a3362e
rutuja9399 <81827038+rutuja9399@users.noreply.github.com>
2026-06-19 17:23:50 +0530

    Update Buyorder.java

--- src/newPackage/Buyorder.java
+++ src/newPackage/Buyorder.java
@@ -3,5 +3,8 @@ package newPackage;
 public class Buyorder {
     public static void main(String[] args) {
         System.out.println("i am new here");
+        System.out.println("You are where");
+
+
     }
 }
